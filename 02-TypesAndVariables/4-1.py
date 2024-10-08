09089d908a8c16a6075fd5bf39515ff571aab272
Weronika Gurba <wg.weronika.gu@gmail.com>
2024-10-08 11:58:56 +0200

    sekcja 4 skończona

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,9 @@
+###
+# Displaying student's personal data
+#
+name = "Adam"
+age = 20
+height = 175
+print(f"My name is {name}.")
+print(f"I am {age} years old and my height is {height}.")
+print(f"In 6 years I will be {age+6} years old.")
