6fbef597264d606ac62019e7ff6bf7304256da9b
Weronika Gurba <wg.weronika.gu@gmail.com>
2024-10-08 11:40:25 +0200

    sekcja 3 skończona

---
+++ 02-TypesAndVariables/3-3.py
@@ -0,0 +1,14 @@
+###
+# A program for swapping two varable values
+#
+x = 7
+y = 34
+z = 0 # additional, auxiliary variable
+print("Before swapping: x=", x, "y=", y)
+
+# swap the values
+x = 34
+y = 7
+z = 0
+
+print("After swapping: x=", x, "y=", y)
---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,6 @@
+import math
+h = input('Enter the height in meters: ')
+h = int(h)
+h = h/1000
+d = 3.57 * math.sqrt(h)
+print(d)
